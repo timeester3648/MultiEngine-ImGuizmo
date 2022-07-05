bb59c99506beab7fa26be95d303c3205d2df4fd4
timeester3648 <45339455+timeester3648@users.noreply.github.com>
2022-07-05 15:29:20 +0200

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,40 @@
+include "../../premake/common_premake_defines.lua"
+
+project "ImGuizmo"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++latest"
+	cdialect "C17"
+	targetname "%{prj.name}"
+	inlining "Auto"
+
+	files {
+		"./**.h",
+		"./**.cpp"
+	}
+
+	includedirs {
+		"%{IncludeDir.imgui}/imgui"
+	}
+
+ 	filter "system:windows"
+		disablewarnings { "4018" }
+
+ 	filter "configurations:Debug"
+		defines { "MLE_DEBUG_BUILD", "DEBUG" }
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
+
+	filter "configurations:Distribution"
+		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
+		flags { "LinkTimeOptimization" }
+		runtime "Release"
+		optimize "speed"
+		intrinsics "on"
