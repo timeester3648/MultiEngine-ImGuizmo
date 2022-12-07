3b35d9d23587551fb4ab3d9a11390859113f6eb0
Tim van de Wetering <45339455+timeester3648@users.noreply.github.com>
2022-12-07 15:23:02 +0100

    Update submodule

--- premake5.lua
+++ premake5.lua
@@ -24,24 +24,5 @@ project "ImGuizmo"
 		"%{IncludeDir.general_includes}"
 	}
 
- 	filter "system:windows"
+	filter "action:vs*"
 		disablewarnings { "4018" }
-
- 	filter "configurations:Debug"
-		defines { "MLE_DEBUG_BUILD", "DEBUG" }
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
-
-	filter "configurations:Distribution"
-		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
