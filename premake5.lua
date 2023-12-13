259ca47a3d5c4f084d62bfe81ef4159518c2ebdf
timeester3648 <weteringt@gmail.com>
2023-12-13 16:39:29 +0100

    Update

--- premake5.lua
+++ premake5.lua
@@ -7,6 +7,36 @@ project "ImGuizmo"
 	cdialect "C17"
 	targetname "%{prj.name}"
 	inlining "Auto"
+	tags { "use_shared_mle" }
+
+	files {
+		"./*.h",
+		"./*.cpp"
+	}
+
+	includedirs {
+		"%{IncludeDir.fmt}",
+		"%{IncludeDir.mle}",
+		"%{IncludeDir.fmt}",
+		"%{IncludeDir.boost}",
+		"%{IncludeDir.imgui}",
+		"%{IncludeDir.spdlog}",
+		"%{IncludeDir.magic_enum}",
+		"%{IncludeDir.intrinsics}",
+		"%{IncludeDir.general_includes}"
+	}
+
+	filter "toolset:msc"
+		disablewarnings { "4018" }
+
+project "ImGuizmoStatic"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++latest"
+	cdialect "C17"
+	targetname "ImGuizmo"
+	inlining "Auto"
+	tags { "use_statuc_mle" }
 
 	files {
 		"./*.h",
